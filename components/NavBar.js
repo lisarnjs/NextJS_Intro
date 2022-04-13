1d25e29b94ce3b99bd56ace21b73a56569bac143
권은서 <kwonenseo@gmail.com>
2022-04-13 11:23:27 +0900

    How to use module.css and classNames
    두개 이상의 classname을 붙히는 방법

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,10 +8,16 @@ export default function NavBar() {
   return (
   <nav>
     <Link href="/">
+    {/* 백틱을 사용하여 두개 이상의 css classname을 붙혀줄 수 있다. */}
     <a className={`${styles.link} ${router.pathname==="/" ? styles.active : ""}`}>Home</a>
     </Link>
     <Link href="/about">
-    <a className={`${styles.link} ${router.pathname==="/about" ? styles.active : ""}`}>About</a>
+    {/* join(" ")으로 공백을 간격으로 한 문자열로서 합쳐 버림 
+    join() => 한 배열을 다른 한 문자열로 바꾸는 방법 */}
+    <a className={[
+      styles.link, 
+      router.pathname==="/about" ? styles.active : ""
+    ].join(" ")}>About</a>
     </Link>
   </nav>
   );
