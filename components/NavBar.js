f1878442413a06ac1c60dd155e093ce241a1599e
권은서 <kwonenseo@gmail.com>
2022-04-13 11:33:14 +0900

    Style CSS in another way
    style jsx를 이용한 조금 더 편리한 방법

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,24 +1,27 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router.pathname);
   return (
   <nav>
     <Link href="/">
-    {/* 백틱을 사용하여 두개 이상의 css classname을 붙혀줄 수 있다. */}
-    <a className={`${styles.link} ${router.pathname==="/" ? styles.active : ""}`}>Home</a>
+    <a className={router.pathname === "/" ? "active" : ""}>Home</a>
     </Link>
     <Link href="/about">
-    {/* join(" ")으로 공백을 간격으로 한 문자열로서 합쳐 버림 
-    join() => 한 배열을 다른 한 문자열로 바꾸는 방법 */}
-    <a className={[
-      styles.link, 
-      router.pathname==="/about" ? styles.active : ""
-    ].join(" ")}>About</a>
+    <a className={router.pathname === "/about" ? "active" : ""}>About</a>
     </Link>
+    <style jsx>{`
+      nav {
+        background-color: tomato;
+      }
+      a {
+        text-decoration: none;
+      }
+      .active {
+        color: white;
+      }
+    `}</style>
   </nav>
   );
 }
