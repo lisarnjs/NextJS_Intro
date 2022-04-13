e4465fc5279ab3d7c685ceaeca4776a6b0d26318
권은서 <kwonenseo@gmail.com>
2022-04-13 13:12:12 +0900

    Use Seo Component for Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,11 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({Component, pageProps}) {
   return (
-    <div>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <span>Hello</span>
-      
-      <style jsx global>{`
-        a {
-          color: yellow;
-        }
-      `}</style>
-    </div>
+    </Layout>
     
   );
 }
