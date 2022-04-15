02639ff8a7bc87ce69b0c403b0c8b1332d4c6b16
권은서 <kwonenseo@gmail.com>
2022-04-15 14:43:22 +0900

    Modified config.js to hide API_KEY

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = "55e16d79303354a36dea5a40d5005033";
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      }
+    ]
+  }, 
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
